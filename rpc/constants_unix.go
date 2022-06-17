b31abf953414d9c9a97a6b7b568954a0c9844dfb
teachain <281477138@qq.com>
2022-06-17 11:34:28 +0800

    duplicate symbol '_max_socket_path_size'

--- rpc/constants_unix.go
+++ rpc/constants_unix.go
@@ -3,8 +3,17 @@
 
 package rpc
 
+/*
+#include <sys/un.h>
+
+int client_max_socket_path_size() {
+struct sockaddr_un s;
+return sizeof(s.sun_path);
+}
+
+*/
 import "C"
 
 var (
-	max_path_size = C.max_socket_path_size()
+	max_path_size = C.client_max_socket_path_size()
 )
